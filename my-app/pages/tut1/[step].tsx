aea79b0b96cf01a2536242553a2d96fd20b09b00
Richard Imaoka <richard.e.imaoka@gmail.com>
2022-11-28 03:36:00 +0900

    put things into const

--- my-app/pages/tut1/[step].tsx
+++ my-app/pages/tut1/[step].tsx
@@ -6,6 +6,33 @@ import { useState } from "react";
 import { ConsoleCommand } from "../../components/ConsoleCommand";
 import { ConsoleResult } from "../../components/ConsoleResult";
 
+const data: Props[] = [
+  {
+    type: "command",
+    command: `mkdir tutorial-docker-cmd-entrypoint
+cd tutorial-docker-cmd-entrypoint`,
+    result: `Using default tag: latest
+latest: Pulling from library/nginx
+a603fa5e3b41: Pull complete
+c39e1cda007e: Pull complete
+90cfefba34d7: Pull complete
+a38226fb7aba: Pull complete
+62583498bae6: Pull complete
+9802a2cfdb8d: Pull complete
+Digest: sha256:e209ac2f37c70c1e0e9873a5f7231e91dcd83fdf1178d8ed36c2ec09974210ba
+Status: Downloaded newer image for nginx:latest
+docker.io/library/nginx:latest`,
+  },
+  {
+    type: "command",
+    command: `cat << EOF > Dockerfile.cmd1 
+FROM ubuntu
+CMD ["echo", "abc"]`,
+    result: `FROM ubuntu
+CMD ["echo", "abc"]`,
+  },
+];
+
 interface Props {
   type: "command";
   command: string;
@@ -18,7 +45,7 @@ interface Params extends ParsedUrlQuery {
 
 export const getStaticPaths: GetStaticPaths<Params> = async () => {
   return {
-    paths: [{ params: { step: "step1" } }],
+    paths: [{ params: { step: "1" } }, { params: { step: "2" } }],
     fallback: false,
   };
 };
@@ -26,24 +53,13 @@ export const getStaticPaths: GetStaticPaths<Params> = async () => {
 export const getStaticProps: GetStaticProps<Props, Params> = async ({
   params,
 }) => {
-  return {
-    props: {
-      type: "command",
-      command: `mkdir tutorial-docker-cmd-entrypoint
-cd tutorial-docker-cmd-entrypoint`,
-      result: `Using default tag: latest
-latest: Pulling from library/nginx
-a603fa5e3b41: Pull complete
-c39e1cda007e: Pull complete
-90cfefba34d7: Pull complete
-a38226fb7aba: Pull complete
-62583498bae6: Pull complete
-9802a2cfdb8d: Pull complete
-Digest: sha256:e209ac2f37c70c1e0e9873a5f7231e91dcd83fdf1178d8ed36c2ec09974210ba
-Status: Downloaded newer image for nginx:latest
-docker.io/library/nginx:latest`,
-    },
-  };
+  const index = Number(params?.step);
+  if (!data[index]) {
+    return { notFound: true };
+  } else {
+    const props = data[index];
+    return { props: props };
+  }
 };
 
 const TutorialStep = ({ type, command, result }: Props) => {
@@ -77,7 +93,7 @@ const TutorialStep = ({ type, command, result }: Props) => {
       {isOpen1 && <ConsoleResult result={result} />}
 
       <div>
-        <Link href="./page2">
+        <Link href="./step2">
           <button type="button">next</button>
         </Link>
       </div>
